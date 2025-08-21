3d44fe1a51a658d356f4bc481a3d73e43046ee4c
init050 <fazelzare@proton.me>
2025-08-21 22:26:15 +0330

    2

--- account_module/urls.py
+++ account_module/urls.py
@@ -7,10 +7,11 @@ app_name = 'account_module'
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(next_page='account_module:login'), name='logout'),    path('profile/', views.UserProfileView.as_view(), name='profile'),
     path('change-password/', views.PasswordChangeView.as_view(), name='change_password'),
-    path('verify-email/<str:token>/', views.EmailVerificationView.as_view(), name='verify_email'),
+    
+    path('email-verification/', views.EmailVerificationView.as_view(), name='email_verification'),
+    path('verify-email/<str:token>/', views.EmailVerificationConfirmView.as_view(), name='verify_email'),
 
     path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
